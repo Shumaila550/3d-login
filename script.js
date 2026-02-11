ca6b9ac30a1f91f728e5a033ed5a169e9a8e263e
Shumaila550 <codecanvas78@gmail.com>
2026-02-12 01:39:33 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const card = document.getElementById("card");
+
+document.addEventListener("mousemove", (e) => {
+    let x = (window.innerWidth / 2 - e.pageX) / 20;
+    let y = (window.innerHeight / 2 - e.pageY) / 20;
+    card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});
+
+document.getElementById("loginForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Login Successful!");
+});
